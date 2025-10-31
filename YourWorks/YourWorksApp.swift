80a8293f8c9385f03f00c803f1e6349dfdbbc1c0
Ali Mohammadzadeh <ali24Mohammadzadeh@gmail.com>
2025-10-31 16:13:05 +0330

    Initial Commit

---
+++ YourWorks/YourWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourWorksApp.swift
+//  YourWorks
+//
+//  Created by Alimmz on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct YourWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
